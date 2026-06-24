af9aaceda5be190996a48abc11d663f5f4446aa3
Vishakha Pathak <vmpathak@andrew.cmu.edu>
2026-06-24 11:16:42 -0400

    remove node_modules and dist from repo

--- node_modules/@paper-design/shaders-react/dist/color-props-are-equal.d.ts
+++
@@ -1,6 +0,0 @@
-interface PropsWithColors {
-    colors?: string[];
-    [key: string]: unknown;
-}
-export declare function colorPropsAreEqual(prevProps: PropsWithColors, nextProps: PropsWithColors): boolean;
-export {};
--- node_modules/@paper-design/shaders-react/dist/transparent-pixel.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const transparentPixel = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
